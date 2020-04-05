c4ac643124b882bd0964652c75c5388450eb255f
brthema072 <brunomarthe@hotmail.com>
2020-04-05 07:30:08 -0300

    adicionando mais componentes inciais e atualizando angluar cli core

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { AgendaFormComponent } from './agenda/agenda-form/agenda-form.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { OndeEstamosComponent } from './onde-estamos/onde-estamos.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo:'home' },
-  { path:'home', component: HomeComponent}
+  { path:'home', component: HomeComponent },
+  { path:'agenda', component: AgendaComponent },
+  { path:'agenda/horaios', component: AgendaFormComponent },
+  { path:'onde-estamos', component: OndeEstamosComponent },
+  { path:'sobre-nos', component: QuemSomosComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { HomeComponent } from './home/home.component';
 import { MaterialModule } from './material.module';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { AgendaFormComponent } from './agenda/agenda-form/agenda-form.component';
+import { OndeEstamosComponent } from './onde-estamos/onde-estamos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ToolBarComponent,
+    QuemSomosComponent,
+    AgendaComponent,
+    AgendaFormComponent,
+    OndeEstamosComponent,
   ],
   imports: [
     BrowserModule,
